c2dac85b1892f1b50bcc6b3036023e261c3ddc17
Zhenfeng.Zou <zhenfeng.zou1998@gmail.com>
2024-05-31 09:56:59 +0800

    :tada: [Fix] 单元测试修正

--- src/test/java/io/hydrocarbon/moutai/service/MetadataServiceTest.java
+++ src/test/java/io/hydrocarbon/moutai/service/MetadataServiceTest.java
@@ -2,6 +2,7 @@ package io.hydrocarbon.moutai.service;
 
 import jakarta.annotation.Resource;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -9,6 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
  * @author Zou Zhenfeng
  * @since 2024-05-22
  */
+@Disabled("用来跑测试数据，并非单元测试功能。")
 @SpringBootTest
 class MetadataServiceTest {
 
